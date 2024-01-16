030b9dbdf424d6975f6984d768ea9bfe339166bb
Alex-wsl2 <abaekey@knights.ucf.edu>
2024-01-16 18:32:31 -0500

    Fixed typing issues, now passes all tests with no lint errors

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,11 +1,14 @@
 // Endpoint for querying the fibonacci numbers
 
 import fibonacci from "./fib";
+import {Request, Response} from 'express'; 
+/* above line needed according to https://stackoverflow.com/questions/58567145/types-for-req-and-res-in-express/58567246#58567246 but worked without it */
 
-export default (req, res) => {
-  const { num } = req.params;
+export default (req:Request, res:Response) => {
+  const num:string = req.params.num
+  const fibN:number = fibonacci(parseInt(num));
+  /* parseInt takes a string arguement */
 
-  const fibN = fibonacci(parseInt(num));
   let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
